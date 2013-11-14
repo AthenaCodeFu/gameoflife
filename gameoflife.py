dad6d06033aa788432c3ab63a8e010c94bebf921
Steven Lu <stevenlu443@gmail.com>
2013-11-14 13:00:44 -0500

    end of this week's session

--- gameoflife.py
+++ gameoflife.py
@@ -1,30 +1,43 @@
 class Generation():
     def __init__(self, cells):
         self.cells = cells
+        self.grid = []
+        for x in range(1):
+            self.grid.append([])
+            for y in range(3):
+                self.grid[x].append(False)
 
-    def gen_grid(self):
-        minx = 1000;
-        miny = 1000;
-        maxx = -1000;
-        maxy = -1000;
-        for cell in self.cells:
-            if (cell[0] < minx):
-                minx = cell[0]
-            if (cell[1] < miny):
-                miny = cell[1]
-            if (cell[0] > maxx):
-                maxx = cell[0]
-            if (cell[1] > maxy):
-                maxy = cell[1]
+        
+#    def gen_grid(self):
+#        minx = 1000;
+#        miny = 1000;
+#        maxx = -1000;
+#        maxy = -1000;
+#        for cell in self.cells:
+#            if (cell[0] < minx):
+#                minx = cell[0]
+#            if (cell[1] < miny):
+#                miny = cell[1]
+#            if (cell[0] > maxx):
+#                maxx = cell[0]
+#            if (cell[1] > maxy):
+#                maxy = cell[1]
+#
+#        grid = []
+#        for x in range(minx, maxx + 1)
+#            for y in range(miny, maxy + 1)
+#                grid[x][y] = false;
+#
+
 
     def ascii_art(self):
         art = ""
-        minx 
-        maxx 
-        miny
-        maxy
-        for 
-        for
-        for cell in self.cells:
-            art += "X"
+        for row in self.grid:
+            for y in row:
+                if row[y]:
+                    art += 'X'
+                else:
+                    art += ' '
+            
+            art += "\n"
         return art
--- test.py
+++ test.py
@@ -4,15 +4,19 @@ import gameoflife
 class DisplayTestCase(unittest.TestCase):
     def test_empty_generation(self):
         generation = gameoflife.Generation([])
-        self.assertEqual("", generation.ascii_art())
+        self.assertEqual("   \n", generation.ascii_art())
 
     def test_one_cell(self):
         generation = gameoflife.Generation([(0, 0)])
-        self.assertEqual("X", generation.ascii_art())
+        self.assertEqual("X\n", generation.ascii_art())
+    
+    def tenbyten(self):
+        pass
 
+        
     def test_two_cells_with_space(self):
         generation = gameoflife.Generation([(0, 0), (2, 0)])
-        self.assertEqual("X X", generation.ascii_art())
+        self.assertEqual("X X\n", generation.ascii_art())
 
 if __name__ == '__main__':
     unittest.main()
